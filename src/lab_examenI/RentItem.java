02cd4dfc4609ceb94b119ce04094cbe769cc4fdf
Alejandr0-Ramirez <alejoramseg@gmail.com>
2026-02-20 08:54:50 -0600

    Clase RentItem

--- src/lab_examenI/RentItem.java
+++ src/lab_examenI/RentItem.java
@@ -1,14 +1,63 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package lab_examenI;
 
-/**
- *
- * @author Fabio Sierra
- */
-public class RentItem {
+import javax.swing.ImageIcon;
+
+//Clase abstracta
+public abstract class RentItem {
     
+    //atributos
+    private String codigo;
+    private String nombre;
+    private double precioBase;
+    private int copiasDisponibles;
+    private ImageIcon imagen;
+    
+    //constructor inicializar datos
+    public RentItem(String codigo, String nombre, double precioBase){
+        this.codigo=codigo;
+        this.nombre=nombre;
+        this.precioBase=precioBase;
+        this.copiasDisponibles=0;
+    }
+    
+    //getters
+    public String getCodigo(){
+        return codigo;
+    }
+    
+    public String getNombre(){
+        return nombre;
+    }
+    
+    public double getPrecioBase(){
+        return precioBase;
+    }
+    
+    public int getCopiasDisponibles(){
+        return copiasDisponibles;
+    }
+    
+    public ImageIcon getImagen(){
+        return imagen;
+    }
+    
+    //setters
+    public void setImagen(ImageIcon imagen){
+        this.imagen=imagen;
+    }
+    
+    public void setCopiasDisponibles(int copiasDisponibles){
+        this.copiasDisponibles=copiasDisponibles;
+    } 
+    
+    //metodo abstracto q implementa movie y game
+    public abstract double pagoRenta(int dias);
+    
+    @Override
+    public String toString(){
+        return "Codigo: " + codigo +
+                "\nNombre: " + nombre +
+                "\nPrecio Base: lps" + precioBase +
+                "\nCopias Disponibles: " + copiasDisponibles;
+    }
 }
